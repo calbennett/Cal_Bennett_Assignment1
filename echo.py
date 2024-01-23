3f459d974e856bf4315560bbd074c6f728dab587
calbennett <156846156+calbennett@users.noreply.github.com>
2024-01-23 14:52:32 -0600

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,2 +1,12 @@
+#echo.py
+def echo(text: str, repititions : int = 3):
+    while repititions >= 1:
+        print(text[repititions * -1:])
+        repititions -= 1
+    return(".")
+    
+if __name__ == "__main__":
+    text = input("Yell something at a mountain: ")
+    print(echo(text))
 
     
