53d5a8685cda3c4d7851a7671a70d3c26c27345e
calbennett <156846156+calbennett@users.noreply.github.com>
2024-01-24 11:57:09 -0600

    Added recursion code

--- fib.py
+++ fib.py
@@ -1,1 +1,11 @@
 #fib.py
+from functools import lru_cache
+import timer
+def fib(n:int) -> int:
+    if n <= 1:
+        return n
+    else:
+        return fib(n-2) + fib(n-1)
+
+if __name__ == "__main__":
+    fib(5)
