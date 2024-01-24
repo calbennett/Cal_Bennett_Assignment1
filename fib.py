e3bb820d94dc43ea2635464eda0617ca33539d43
calbennett <156846156+calbennett@users.noreply.github.com>
2024-01-24 14:36:51 -0600

    Added timer functionality.

--- fib.py
+++ fib.py
@@ -5,10 +5,14 @@ memory = {}
 def fib(n:int) -> int:
     if n <= 1:
         return n
-    print(n)
     if n not in memory:
         memory[n] = fib(n-1) + fib(n-2)
     return memory[n]
 
 if __name__ == "__main__":
+    t = timer.timer()
+    t._start('Starting...')
     print(fib(100))
+    t._stop('Done')
+    
+    print(t._elapse)
