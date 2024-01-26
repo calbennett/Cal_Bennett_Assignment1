69b93ff458269af73fa7eb02d0dc949bc6852768
calbennett <calbennett@uiowa.edu>
2024-01-26 15:24:45 -0600

    Added a second timer to show time for each computation of n.

--- fib.py
+++ fib.py
@@ -3,10 +3,17 @@ from functools import lru_cache
 import timer
 memory = {}
 def fib(n:int) -> int:
+    t2 = timer.timer()
+    t2._start('')
     if n <= 1:
+        t2._stop('')
+        print('Elapsed time for n =',n,':',t2._elapse * 1000, 'milliseconds')
         return n
+        
     if n not in memory:
         memory[n] = fib(n-1) + fib(n-2)
+    t2._stop('')
+    print('Elapsed time for n =',n,':',t2._elapse * 1000, 'milliseconds')
     return memory[n]
 
 if __name__ == "__main__":
@@ -15,4 +22,4 @@ if __name__ == "__main__":
     print(fib(100))
     t._stop('')
     
-    print('Elapsed time:', t._elapse, 'seconds')
+    print('Total elapsed time:', t._elapse, 'seconds')
