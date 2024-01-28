5eac20d249bdd57c1e1dcaf26a6dc356a1f93ee6
calbennett <calbennett@uiowa.edu>
2024-01-28 12:32:37 -0600

    Added csv value to track times and code to write to csv.

--- fib.py
+++ fib.py
@@ -1,6 +1,7 @@
 #fib.py
 from functools import lru_cache
 import timer
+import csv
 memory = {}
 def fib(n:int) -> int:
     t2 = timer.timer()
@@ -8,15 +9,21 @@ def fib(n:int) -> int:
     if n <= 1:
         t2._stop('')
         print('Elapsed time for n =',n,':',t2._elapse * 1000, 'milliseconds')
+        data = [int(n), float(t2._elapse) * 1000]
+        writer.writerow(data)
         return n
         
     if n not in memory:
         memory[n] = fib(n-1) + fib(n-2)
     t2._stop('')
     print('Elapsed time for n =',n,':',t2._elapse * 1000, 'milliseconds')
+    data = [int(n), float(t2._elapse) * 1000]
+    writer.writerow(data)
     return memory[n]
 
 if __name__ == "__main__":
+    f = open('times.csv', 'w')
+    writer = csv.writer(f)
     t = timer.timer()
     t._start('')
     print(fib(100))
